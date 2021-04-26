0461f7c9928603bdf86f37b210fbd845685c78cc
Takeyuki Tamura <takeyuki.tamura@gmail.com>
2021-04-26 11:16:52 +0900

    Add files via upload

---
+++ example3.m
@@ -0,0 +1,13 @@
+function [outputArg1,outputArg2] = example3()
+%example2 calculates the gene deletion strategy for growth coupling
+%for pantothenate in iML1515.
+%
+% Apr. 23, 2021  Takeyuki TAMURA
+%
+load('iML1515.mat');
+model=iML1515;
+[gr pr it success]=gDel_minRN(model,model.mets{423},10,0.1,0.1);
+
+save('example3.mat');
+end
+
